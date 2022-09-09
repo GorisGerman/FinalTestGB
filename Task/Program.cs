e32cd880a836516455f105e7e2f1595418e52193
Goris German <mefisto404@gmail.com>
2022-09-09 14:21:09 +0300

    programm + readme fix

--- Task/Program.cs
+++ Task/Program.cs
@@ -38,3 +38,32 @@ string[] NewStringArrayByLength(string[] stringArray, int lengthLimit)
     }
     return newStringArray;
 }
+
+
+string yesNo = string.Empty;
+Console.Write("Вводим массив с клавиатуры? yes/no: ");
+yesNo = Console.ReadLine();
+
+if (yesNo.ToLower() == "yes")
+{
+    Console.WriteLine("Введите размер массива: ");
+    int size = Convert.ToInt32(Console.ReadLine());
+    string[] array = NewStringArrayInput(size);
+    Console.WriteLine('[' + string.Join(", ", array) + ']');
+
+    Console.WriteLine("Максимум символов в элементе нового массива?: ");
+    int length = Convert.ToInt32(Console.ReadLine());
+    string[] newArray = NewStringArrayByLength(array, length);
+    Console.WriteLine('[' + string.Join(", ", newArray) + ']');
+}
+
+else
+{
+    string[] defaultStringArray = { "hello", "2", "world", ":-)" };
+    Console.WriteLine('[' + string.Join(", ", defaultStringArray) + ']');
+
+    Console.WriteLine("Максимум символов в элементе нового массива?: ");
+    int length = Convert.ToInt32(Console.ReadLine());
+    string[] newArray = NewStringArrayByLength(defaultStringArray, length);
+    Console.WriteLine('[' + string.Join(", ", newArray) + ']');
+}
